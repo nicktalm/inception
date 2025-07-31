1d02c165bb8e974579b4f79da3420ab3d46d4b6c
Nick Talmon <ntalmon@1-A-16.42heilbronn.de>
2025-07-31 14:39:29 +0200

    working on mariadb

--- srcs/requirements/mariadb/conf/start_mariadb.sh
+++ srcs/requirements/mariadb/conf/start_mariadb.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
 
+# Datenbank initialisieren, falls leer
+if [ ! -d /var/lib/mysql/mysql ]; then
+  echo "==> Initialisiere Datenbank..."
+  mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
+
+  # Temporärer Start
+  mysqld --user=mysql --bootstrap <<EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
+EOF
+fi
+
+echo "==> Starte MariaDB..."
+exec mysqld --user=mysql --console
