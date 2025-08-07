3c65574f43a319c5f3987a698ca16da0d4a87252
Nick Talmon <ntalmon@1-A-16.42heilbronn.de>
2025-08-07 16:08:59 +0200

    small fix

--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -9,46 +9,48 @@
 	wget https://wordpress.org/latest.tar.gz
 	tar -xzf latest.tar.gz
 	mv wordpress/* /var/www/html/
-	rm latest.tar.gz
+	rm -rf latest.tar.gz wordpress
 
 	cd /var/www/html
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
-	./wp-cli.phar core download \
-			--allow-root
-	/wp-cli.phar config create \
-			--dbname=$MARIADB_DATABASE \
-			--dbuser=$MARIADB_USER \
-			--dbpass=$MARIADB_PASSWORD \
-			--dbhost="mariadb" \
-			--allow-root
 
-			
+	# Entferne den Download – du hast WordPress ja schon entpackt
+	# ./wp-cli.phar core download --allow-root
+
+	./wp-cli.phar config create \
+		--dbname=$MARIADB_DATABASE \
+		--dbuser=$MARIADB_USER \
+		--dbpass=$MARIADB_PASSWORD \
+		--dbhost="mariadb" \
+		--allow-root
+
 	# Debugging
 	echo "Admin-Benutzername: $WORDPRESS_ADMIN_USER"
 
-	# Bedingung für Admin-Benutzernamen
 	if [[ $WORDPRESS_ADMIN_USER == *admin* || $WORDPRESS_ADMIN_USER == *Admin* ]]; then
 		echo "Der Admin-Benutzername darf nicht 'admin' oder 'Admin' enthalten."
 		exit 1
 	fi
 
 	./wp-cli.phar core install \
-			--url=localhost \
-			--title=inception \
-			--admin_user=$WORDPRESS_ADMIN_USER \
-			--admin_password=$WORDPRESS_ADMIN_PASSWORD \
-			--admin_email=$WORDPRESS_ADMIN_EMAIL \
-			--allow-root
+		--url=localhost \
+		--title=inception \
+		--admin_user=$WORDPRESS_ADMIN_USER \
+		--admin_password=$WORDPRESS_ADMIN_PASSWORD \
+		--admin_email=$WORDPRESS_ADMIN_EMAIL \
+		--allow-root
+
 	./wp-cli.phar user create \
-			$WORDPRESS_TEST_USER \
-			$WORDPRESS_TEST_USER_EMAIL \
-			--user_pass=$WORDPRESS_TEST_USER_PASSWORD \
-			--role=author \
-			--path=/var/www/html/ \
-			--allow-root
-	./wp-cli.phar theme activate twentytwentyfour \
-			--allow-root
+		$WORDPRESS_TEST_USER \
+		$WORDPRESS_TEST_USER_EMAIL \
+		--user_pass=$WORDPRESS_TEST_USER_PASSWORD \
+		--role=author \
+		--path=/var/www/html/ \
+		--allow-root
+
+	./wp-cli.phar theme activate twentytwentyfour --allow-root
+
 # fi
 
 php-fpm8.2 -F
